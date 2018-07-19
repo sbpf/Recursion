3be98284b03648bf0e430348296681cb7e80ffa6
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-07-18 17:19:47 -0700

    factorial

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+
+	public static int factorial(int num)
+	{
+		if(num==0)
+			return 1;
+		return num*factorial(num-1);
+	}
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+
+	}
+
+}
